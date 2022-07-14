829b2c340ad370b7daba34ea4a73bb2eed7b6140
Johnny Lorran <johnnylsrdarocha@gmail.com>
2022-07-14 00:54:17 -0300

    teste heroku

--- backend/src/main/java/com/projetodev/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/projetodev/dsmeta/services/SmsService.java
@@ -46,7 +46,7 @@ public class SmsService {
 		String msg = "O vendedor " + sale.getSellerName()
 				+ " foi destaque em " + mes + " / "
 				+ sale.getDate().getYear() + " com um total de R$ " 
-				+ String.format("%0.2f", sale.getAmount()) + " ." ;
+				+ String.format("%.2f", sale.getAmount()) + " ." ;
 
 		Twilio.init(twilioSid, twilioKey);
 
