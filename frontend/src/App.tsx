e4944ec5c8f64b1f7b52d27cef8c189eb7827f44
Johnny Lorran <johnnylsrdarocha@gmail.com>
2022-07-16 00:01:16 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import HeaderClass from "./components/Header";
 import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
+      <ToastContainer />
       <HeaderClass />
-
       <main>
         <section id="sales">
           <div className="dsmeta-container">
@@ -16,7 +18,6 @@ function App() {
       </main>
     </>
   )
-
 }
 
 export default App
